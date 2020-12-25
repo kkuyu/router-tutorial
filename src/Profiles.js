fcb326b35abf4a29dd4b4798f5f06d7ebc37e9c8
ohsam <end2000n@naver.com>
2020-12-25 20:50:46 +0900

    서브라우트

---
+++ src/Profiles.js
@@ -0,0 +1,28 @@
+import Profile from "./Profile";
+import { Link, Route } from "react-router-dom";
+
+function Profiles() {
+	const lorem = "lorem ipsum dolor sit amet";
+	return (
+		<div>
+			<h3>사용자 목록</h3>
+			<ul>
+				<li>
+					<Link to="/profiles/kku">kku</Link>
+				</li>
+				<li>
+					<Link to="/profiles/homer">homer</Link>
+				</li>
+			</ul>
+
+			<Route
+				path="/profiles"
+				render={() => <div>사용자를 선택해주세요 {lorem}</div>}
+				exact
+			/>
+			<Route path="/profiles/:username" component={Profile} />
+		</div>
+	);
+}
+
+export default Profiles;
